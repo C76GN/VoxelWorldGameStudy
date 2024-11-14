ba83f2f04e6789956407dd2a6dd1eb75cc3c2861
C76GN <cl7o6dgyn@gmail.com>
2024-11-14 21:27:31 +0800

    Use URP

--- Assets/_Script/Chunk.cs
+++ Assets/_Script/Chunk.cs
@@ -1,7 +1,25 @@
 ﻿using System;
+using UnityEngine;
 
 public static class Chunk
 {
+    public static void LoopThroughTheBlocks(ChunkData chunkData, Action<int, int, int> actionToPerform)
+    {
+        for(int index = 0; index < chunkData.blocks.Length; index++)
+        {
+            var position = GetPostitionFromIndex(chunkData, index);
+            actionToPerform(position.x, position.y, position.z);
+        }
+    }
+
+    private static Vector3Int GetPostitionFromIndex(ChunkData chunkData, int index)
+    {
+        int x = index % chunkData.chunkSize;
+        int y=(index/chunkData.chunkSize)%chunkData.chunkHeight;
+        int z=index/(chunkData.chunkSize*chunkData.chunkHeight);
+        return new Vector3Int(x, y, z);
+    }
+
     public static MeshData GetChunkMeshData(ChunkData chunkData)
     {
         MeshData meshData = new MeshData(true);
